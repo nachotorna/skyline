a02871f80d5efacb7374c93e81d738e380707467
nachotorna <132461024+nachotorna@users.noreply.github.com>
2024-08-13 16:41:17 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+  
+  function toggleNightMode() {
+    document.body.classList.add('night-mode');
+    document.getElementById('moon-button').style.display = 'none';
+    document.getElementById('sun-button').style.display = 'inline-block';
+  }
+
+  function toggleDayMode() {
+    document.body.classList.remove('night-mode');
+    document.getElementById('moon-button').style.display = 'inline-block';
+    document.getElementById('sun-button').style.display = 'none';
+  }
+
+
+ 
